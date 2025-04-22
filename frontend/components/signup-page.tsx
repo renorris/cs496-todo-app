822ce14425c982df30ba15b23c724027c887aa48
Reese Norris <reese.norris@gmail.com>
2025-04-22 15:26:39 -0700

    fix signup auth, basic frontend auth context

--- frontend/components/signup-page.tsx
+++ frontend/components/signup-page.tsx
@@ -25,7 +25,7 @@ export default function SignupPage() {
     e.preventDefault()
 
     try {
-      const response = await fetch("http://localhost:8000/api/user/create", {
+      const response = await fetch("https://todoapp.reesenorr.is/api/user/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
