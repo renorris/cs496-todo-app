f19ddbfb9ecfb07b0a9d98684baf525923cf2d22
Reese Norris <reese.norris@gmail.com>
2025-04-23 13:10:32 -0700

    tweak header

--- frontend/components/dashboard-header.tsx
+++ frontend/components/dashboard-header.tsx
@@ -26,9 +26,21 @@ export function DashboardHeader() {
           <h2 className="text-xl font-bold">TaskDocker</h2>
         </div>
         <div className="flex items-center gap-4">
-          <Button variant="ghost" size="icon" aria-label="Notifications">
-            <Bell className="h-5 w-5" />
-          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" aria-label="Notifications">
+                <Bell className="h-5 w-5" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DropdownMenuLabel className="font-normal">
+                <div className="flex flex-col space-y-1">
+                  <p className="text-sm font-medium leading-none">Nothing to see here!</p>
+                </div>
+              </DropdownMenuLabel>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-8 w-8 rounded-full">
@@ -45,15 +57,6 @@ export function DashboardHeader() {
                   <p className="text-xs leading-none text-muted-foreground">{auth.user ? auth.user.email : ""}</p>
                 </div>
               </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <User className="mr-2 h-4 w-4" />
-                <span>Profile</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Settings className="mr-2 h-4 w-4" />
-                <span>Settings</span>
-              </DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem onClick={() => router.push("/")}>
                 <LogOut className="mr-2 h-4 w-4" />
